d24ba8102c91e9b928c4417a35f209a716f1eba0
Anandh-dev <anandh3103@outlook.com>
2026-04-04 22:23:44 +0530

    backend ready

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 
 // MySQL connection using environment variables
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "127.0.0.1",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "0206",
-  database: process.env.DB_NAME || "expense_db",
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 // Test connection
